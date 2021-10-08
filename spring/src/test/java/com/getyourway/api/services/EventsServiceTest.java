3574d52998330980fbdfe6b11f33ee19989a6ac7
Ryan Lee <ryan.lee2@sky.uk>
2021-10-08 17:59:23 +0100

    Started eventsService unit testing

--- spring/src/test/java/com/getyourway/api/services/EventsServiceTest.java
+++ spring/src/test/java/com/getyourway/api/services/EventsServiceTest.java
@@ -1,7 +1,31 @@
 package com.getyourway.api.services;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EventsServiceTest {
+
+    static double latitude;
+    static double longitude;
+
+    @BeforeAll
+    static void setup() {
+        latitude = 51.509865;
+        longitude= 0.696969;
+    }
+
+    @AfterAll
+    static void tearDown() {
+    }
+
+    @Test
+    public void getEventsWithin5km_Coordinates_String() {
+
+    }
+
+
 
 }
