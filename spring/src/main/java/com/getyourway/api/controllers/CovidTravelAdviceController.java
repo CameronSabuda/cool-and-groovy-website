00f783586009f42312a05b70fe262d5731229a23
Ryan Lee <ryan.lee2@sky.uk>
2021-10-10 16:13:24 +0100

    Covid Risk controller and service completed, but error with GSON parsing

--- spring/src/main/java/com/getyourway/api/controllers/CovidTravelAdviceController.java
+++ spring/src/main/java/com/getyourway/api/controllers/CovidTravelAdviceController.java
@@ -1,14 +1,29 @@
+
 package com.getyourway.api.controllers;
 
 import com.getyourway.api.services.CovidTravelAdviceService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+
+@RestController
 public class CovidTravelAdviceController {
 
     private CovidTravelAdviceService service;
 
     public CovidTravelAdviceController(CovidTravelAdviceService service) {
         this.service = service;
+    }
+
+    @GetMapping("/covid")
+    public String covid(@RequestParam double latitude, @RequestParam double longitude) {
+        try {
+            return service.getCovidRiskLevel(latitude, longitude);
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- spring/src/main/java/com/getyourway/api/services/CovidTravelAdviceService.java
+++ spring/src/main/java/com/getyourway/api/services/CovidTravelAdviceService.java
@@ -1,7 +1,67 @@
 package com.getyourway.api.services;
 
+import com.google.gson.*;
+import okhttp3.*;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class CovidTravelAdviceService {
+
+    public String getCovidRiskLevel(double latitude, double longitude) throws IOException {
+        String countryCode = convertCoordinates(latitude, longitude);
+
+        JsonObject unfilteredJsonObject = fetchCovidRiskLevel(countryCode);
+
+        JsonObject filteredJsonObject = filterExternalJsonObject(unfilteredJsonObject, countryCode);
+
+        return filteredJsonObject.toString();
+    }
+
+    public JsonObject fetchCovidRiskLevel(String countryCode) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(String.format("https://www.travel-advisory.info/api?countrycode=%s", countryCode))
+                .get()
+                .build();
+
+        ResponseBody responseBody = client.newCall(request).execute().body();
+        String responseBodyString = null != responseBody ? responseBody.string() : "{}";
+
+        JsonElement unfilteredJsonElement = new Gson().fromJson(responseBodyString, JsonElement.class);
+        return unfilteredJsonElement.getAsJsonObject();
+    }
+
+    public JsonObject filterExternalJsonObject(JsonObject unfilteredJsonObject, String countryCode) {
+        JsonObject unfilteredCovidDataObject = unfilteredJsonObject
+                .get("data").getAsJsonObject();
+        // .get(countryCode).getAsJsonObject()
+        // .get("advisory").getAsJsonObject();
+
+        // JsonObject filteredCovidDataObject = new JsonObject();
+        //
+        // filteredCovidDataObject.add("risk_level", unfilteredCovidDataObject.get("score"));
+        // filteredCovidDataObject.add("updated_time", unfilteredCovidDataObject.get("updated"));
+        //
+        // return filteredCovidDataObject;
+        return unfilteredJsonObject;
+    }
+
+
+    public String convertCoordinates(double latitude, double longitude) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(String.format("http://api.geonames.org/countryCode?lat=%f&lng=%f&username=ryansky&radius=5", latitude, longitude))
+                .get()
+                .build();
+
+        ResponseBody responseBody = client.newCall(request).execute().body();
+        // String responseBodyString = null != responseBody ? responseBody.string() : "{}";
+        String responseBodyString = responseBody.string();
+
+        return responseBodyString;
+    }
 }
