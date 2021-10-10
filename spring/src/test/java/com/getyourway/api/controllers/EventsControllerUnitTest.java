c57d259802895ce5ceca0bcacc9269b829e4c301
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-10 13:13:37 +0100

    refactored the Unit Testing class for EventsController

--- spring/src/test/java/com/getyourway/api/controllers/EventsControllerUnitTest.java
+++ spring/src/test/java/com/getyourway/api/controllers/EventsControllerUnitTest.java
@@ -19,14 +19,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.*;
 
 //@ExtendWith(SpringExtension.class)
 //@WebMvcTest(EventsController.class)
-public class EventsControllerTest {
+public class EventsControllerUnitTest {
 
 //    @MockBean
 //    EventsService eventsService;
@@ -37,18 +37,18 @@ public class EventsControllerTest {
 //    @Autowired
 //    ObjectMapper mapper;
 
-    static double londonLatitude;
-    static double londonLongitude;
+    static double latitude_1, latitude_2;
+    static double longitude_1, longitude_2;
 
     @BeforeAll
     static void setUp() {
-        londonLatitude = 51.509865;
-        londonLongitude= -0.118092;
+
     }
 
     @AfterAll
     static void tearDown() {
-
+//        latitude_1 = 0;
+//        longitude_1 = 0;
     }
 
 //    @Test
@@ -66,14 +66,20 @@ public class EventsControllerTest {
 //    }
 
     @Test
-    public void getEventsWithin5km() throws IOException {
+    public void getEventsWithin5km_givenValidCoordinates_shouldReturnTheSameStringTheEventServiceReturns() throws IOException {
+        //arrange
+        latitude_1 = 51.509865;
+        longitude_1= -0.118092;
 
-        String expected = "Hello";
-        EventsService eventsService = Mockito.mock(EventsService.class);
-        when(eventsService.getEventsWithin5km(londonLatitude, londonLongitude)).thenReturn(expected);
+        EventsService eventsService = mock(EventsService.class);
+        when(eventsService.getEventsWithin5km(latitude_1, longitude_1)).thenReturn("Bonfire Night Firework Displays in London");
+
+        //act
         EventsController eventsController = new EventsController(eventsService);
-        assertEquals(expected, eventsController.getEventsWithin5km(londonLatitude, londonLongitude));
+        String actual = eventsController.getEventsWithin5km(latitude_1, longitude_1);
+
+        //assert
+        String expected = "Bonfire Night Firework Displays in London";
+        assertEquals(expected, actual);
     }
-
-
 }
