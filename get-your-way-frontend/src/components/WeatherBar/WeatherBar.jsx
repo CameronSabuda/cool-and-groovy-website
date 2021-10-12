164dbcff5c1d642a9fb812d069a29ba6df6962a5
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-12 13:18:31 +0100

    enabled weather json data parsing and display in WeatherBar and WeatherTile components

--- get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
+++ get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./WeatherBar.css";
 import WeatherTile from "./WeatherTile/WeatherTile";
+import weatherData from "./weatherData.json";
 
 const WeatherBar = () => {
-  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-
   return (
     <div className="WeatherBar">
-      {days.map((day) => {
-        return <WeatherTile day={day} />;
+      {weatherData.weather.map((weather) => {
+        return <WeatherTile data={weather} />;
       })}
     </div>
   );
--- get-your-way-frontend/src/components/WeatherBar/WeatherTile/WeatherTile.jsx
+++ get-your-way-frontend/src/components/WeatherBar/WeatherTile/WeatherTile.jsx
@@ -2,53 +2,55 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import "./WeatherTile.css";
 import { Row, Col, Container } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-const WeatherTile = (props) => {
+const WeatherTile = ({ data }) => {
   return (
     <div>
       <Card>
-        <Card.Header>{props.day}</Card.Header>
+        <Card.Header>{data.day}</Card.Header>
         <Card.Body>
           <Container>
             <Row>
               <img
-                src="http://openweathermap.org/img/wn/10d@4x.png"
-                alt="light showers"
+                src={`http://openweathermap.org/img/wn/${data.icon}@4x.png`}
+                alt="Type of weather"
               />
             </Row>
             <Row>
-              <Card.Text className="temperature-text">23°C</Card.Text>
+              <Card.Text className="temperature-text">{data.temp}°C</Card.Text>
             </Row>
             <Row className="rain-chance-text-container">
-              <Col className="rain-chance-text">
-                <p>37%</p>
-              </Col>
+              <Col className="weather-type-text">{data.description}</Col>
               <Col>
-                <img src="http://openweathermap.org/img/wn/09d.png" alt="" />
+                <label class="rain-chance-text" for="cloud-icon">
+                  {data.chance_of_rain}%
+                </label>
+                <img
+                  id="cloud-icon"
+                  src="http://openweathermap.org/img/wn/09d.png"
+                  alt=""
+                />
               </Col>
             </Row>
           </Container>
         </Card.Body>
       </Card>
-
-      {/* <thead></thead>
-      <tbody>
-        <tr>
-          <td>Tuesday</td>
-          <td>
-            <img src="http://openweathermap.org/img/wn/10d@4x.png" alt="" />
-          </td>
-        </tr>
-        <tr font-size="">23°C</tr>
-        <tr>
-          <td>
-            <img src="./src/Assets/rain.png" alt="" />
-          </td>
-          <td>37%</td>
-        </tr>
-      </tbody> */}
     </div>
   );
 };
 
+WeatherTile.propTypes = {
+  data: PropTypes.shape({
+    date: PropTypes.number,
+    day: PropTypes.string.isRequired,
+    temp: PropTypes.number,
+    chance_of_rain: PropTypes.number,
+    main: PropTypes.string,
+    description: PropTypes.string,
+    icon: PropTypes.string,
+  }),
+};
+
+// requiredFunc: PropTypes.func.isRequired,
 export default WeatherTile;
