f8d7d3ef7899598890623e25cce36799b070d98d
Ryan Lee <ryan.lee2@sky.uk>
2021-10-15 15:44:51 +0100

    Fixed destructuring of reponse.data

--- get-your-way-frontend/src/components/EventsBar/EventsBar.jsx
+++ get-your-way-frontend/src/components/EventsBar/EventsBar.jsx
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from "react";
-import EventsTile from './EventsTile/EventsTile';
-import axios from 'axios';
-import { CardGroup } from "react-bootstrap";
-
+import React, { useEffect, useState } from 'react'
+import EventsTile from './EventsTile/EventsTile'
+import axios from 'axios'
+import { CardGroup } from 'react-bootstrap'
 
 const client = axios.create({
-    baseURL: "http://99.81.186.138:9090/events?latitude=51.5074&longitude=-0.1278"
+	baseURL: 'http://99.81.186.138:9090/events?latitude=51.5074&longitude=-0.1278',
 })
 
 const EventsBar = () => {
-    const [events, setEvents] = useState(null)
+	const [eventsData, setEventsData] = useState()
 
-    useEffect(() => {
-        const getEvents =  async () => {
-            let response = await client.get()
-            let {responseData} = response.data
-            setEvents(responseData)
-        }
-        getEvents()
-    }, [])
+	useEffect(() => {
+		const getEvents = async () => {
+			let response = await client.get()
+			let { events } = response.data
+			setEventsData(events)
+		}
+		getEvents()
+	}, [])
 
-
-    return (
-        <div className= 'EventsBar bg-secondary'>
-            <CardGroup className='d-flex flex-row'>
-                {events && events.map((event, i) => {
-                    return <EventsTile data={event} key={i}/>
-                })}
-            </CardGroup>
-        </div>
-    )
+	return (
+		<div className='EventsBar bg-secondary'>
+			<CardGroup className='d-flex flex-row'>
+				{eventsData &&
+					eventsData.map((event, i) => {
+						return <EventsTile data={event} key={i} />
+					})}
+			</CardGroup>
+		</div>
+	)
 }
 
-export default EventsBar;
+export default EventsBar
--- get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
+++ get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from "react";
-import "./WeatherBar.css";
-import WeatherTile from "./WeatherTile/WeatherTile";
+import React, { useEffect, useState } from 'react'
+import './WeatherBar.css'
+import WeatherTile from './WeatherTile/WeatherTile'
 import axios from 'axios'
 
-
 const client = axios.create({
-  baseURL: "http://99.81.186.138:9090/weather?latitude=51.5074&longitude=-0.1278"
+	baseURL: 'http://99.81.186.138:9090/weather?latitude=51.5074&longitude=-0.1278',
 })
 
 const WeatherBar = () => {
-  const [weatherData, setWeatherData] = useState()
+	const [weatherData, setWeatherData] = useState()
 
-  useEffect(() => {
-    const getWeather = async () => {
-      let response = await client.get()
-      let { weather } = response.data
-      setWeatherData(weather)
-    }
-    getWeather()
-  }, [])
+	useEffect(() => {
+		const getWeather = async () => {
+			let response = await client.get()
+			let { weather } = response.data
+			setWeatherData(weather)
+		}
+		getWeather()
+	}, [])
 
-  return(
-    <div className="WeatherBar">
-      {weatherData && weatherData.map((weather, i) => {
-        return <WeatherTile key={i} data={weather}/>
-      })}
-    </div>
-  )
-};
+	return (
+		<div className='WeatherBar'>
+			{weatherData &&
+				weatherData.map((weather, i) => {
+					return <WeatherTile key={i} data={weather} />
+				})}
+		</div>
+	)
+}
 
-export default WeatherBar;
+export default WeatherBar
