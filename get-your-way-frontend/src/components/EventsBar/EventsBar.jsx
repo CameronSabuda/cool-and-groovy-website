5f6a2b5a6b7a58be02397dc500a60e7ee54f0e8a
Ryan Lee <ryan.lee2@sky.uk>
2021-10-15 16:35:13 +0100

    Added a loading component, whilst waiting for axios request to finish for the EventsBar component

--- get-your-way-frontend/src/components/EventsBar/EventsBar.jsx
+++ get-your-way-frontend/src/components/EventsBar/EventsBar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import EventsTile from './EventsTile/EventsTile'
 import axios from 'axios'
 import { CardGroup } from 'react-bootstrap'
+import EventsLoader from './Loader'
 
 const client = axios.create({
 	baseURL: 'http://99.81.186.138:9090/events?latitude=51.5074&longitude=-0.1278',
@@ -9,20 +10,24 @@ const client = axios.create({
 
 const EventsBar = () => {
 	const [eventsData, setEventsData] = useState()
+	const [isLoading, setLoading] = useState(true)
 
 	useEffect(() => {
 		const getEvents = async () => {
 			let response = await client.get()
 			let { events } = response.data
+
 			setEventsData(events)
+			setLoading(false)
 		}
 		getEvents()
 	}, [])
 
 	return (
 		<div className='EventsBar bg-secondary'>
+			{isLoading && <EventsLoader />}
 			<CardGroup className='d-flex flex-row'>
-				{eventsData &&
+				{!isLoading &&
 					eventsData.map((event, i) => {
 						return <EventsTile data={event} key={i} />
 					})}
