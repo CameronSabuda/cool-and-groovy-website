7cdb85496c36838c955622840db1d023ed73f87a
Cameron <cameronsabuda@gmail.com>
2021-10-15 11:50:03 +0100

    Remove ports in cross origin definition

--- spring/src/main/java/com/getyourway/api/controllers/WeatherController.java
+++ spring/src/main/java/com/getyourway/api/controllers/WeatherController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = {"http://99.81.186.138:9090", "http://127.0.0.1:9090"})
+@CrossOrigin(origins = {"http://99.81.186.138", "http://127.0.0.1"})
 public class WeatherController {
     private WeatherService service;
 
