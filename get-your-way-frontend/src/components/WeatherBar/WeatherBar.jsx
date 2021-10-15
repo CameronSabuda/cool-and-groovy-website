91d38186e231110a0ccc4f5147b7dfc49c9282b9
Cameron <cameronsabuda@gmail.com>
2021-10-15 11:42:30 +0100

    replace localhost with public ip

--- get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
+++ get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 
 const client = axios.create({
-  baseURL: "http://localhost:9090/weather?latitude=51.5074&longitude=-0.1278"
+  baseURL: "http://99.81.186.138:9090/weather?latitude=51.5074&longitude=-0.1278"
 })
 
 const WeatherBar = () => {
