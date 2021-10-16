25a40836e0357cdf1fb40dd053bd4a196c01e40c
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-16 18:00:19 +0100

    put contents into 2 columns, but had layout issue

--- get-your-way-frontend/src/components/Footer/Footer.jsx
+++ get-your-way-frontend/src/components/Footer/Footer.jsx
@@ -3,18 +3,20 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="footer fixed-bottom">
       <Container>
         <hr />
         <Row>
           <Col>
             <p>
               &copy;Get Your Way {new Date().getFullYear()}. All rights
-              reserved. <a href="#privacy-page">Privacy</a> |{" "}
-              <a href="#site-map">Sitemap</a>
+              reserved.
             </p>
           </Col>
-          <Col></Col>
+          <Col>
+            <a href="#privacy-page">Privacy</a> |{" "}
+            <a href="#site-map">Sitemap</a>
+          </Col>
         </Row>
       </Container>
     </div>
