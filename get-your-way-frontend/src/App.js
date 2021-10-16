ab2ba1e81921396cce0682daab65fd11653c395b
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-16 10:27:06 +0100

    Added responsive NavigationBar component, styling and web links to be updated

--- get-your-way-frontend/src/App.js
+++ get-your-way-frontend/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import React from 'react'
-import WeatherBar from './components/WeatherBar/WeatherBar.jsx';
+import "./App.css";
+import React from "react";
+import WeatherBar from "./components/WeatherBar/WeatherBar.jsx";
+import NavigationBar from "./components/NavigationBar/NavigationBar.jsx";
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <WeatherBar />
     </div>
   );
--- get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
+++ get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
@@ -1,32 +1,34 @@
 import React, { useEffect, useState } from "react";
 import "./WeatherBar.css";
 import WeatherTile from "./WeatherTile/WeatherTile";
-import axios from 'axios'
-
+import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://99.81.186.138:9090/weather?latitude=51.5074&longitude=-0.1278"
-})
+  baseURL:
+    "http://99.81.186.138:9090/weather?latitude=51.5074&longitude=-0.1278"
+  // baseURL: "http://localhost:9090/weather?latitude=51.5074&longitude=-0.1278"
+});
 
 const WeatherBar = () => {
-  const [weatherData, setWeatherData] = useState()
+  const [weatherData, setWeatherData] = useState();
 
   useEffect(() => {
     const getWeather = async () => {
-      let response = await client.get()
-      let { weather } = response.data
-      setWeatherData(weather)
-    }
-    getWeather()
-  }, [])
+      let response = await client.get();
+      let { weather } = response.data;
+      setWeatherData(weather);
+    };
+    getWeather();
+  }, []);
 
-  return(
+  return (
     <div className="WeatherBar">
-      {weatherData && weatherData.map((weather, i) => {
-        return <WeatherTile key={i} data={weather}/>
-      })}
+      {weatherData &&
+        weatherData.map((weather, i) => {
+          return <WeatherTile key={i} data={weather} />;
+        })}
     </div>
-  )
+  );
 };
 
 export default WeatherBar;
