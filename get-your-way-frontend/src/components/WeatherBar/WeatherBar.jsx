8793d855f81c1a4340d4f5b9910c2c86e48541c9
Ryan Lee <ryan.lee2@sky.uk>
2021-10-16 22:27:15 +0100

    Removed localhost from url

--- get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
+++ get-your-way-frontend/src/components/WeatherBar/WeatherBar.jsx
@@ -6,7 +6,7 @@ import { Container, Row } from 'react-bootstrap'
 import './WeatherBar.css'
 
 const client = axios.create({
-	baseURL: 'http://localhost:9090/weather?latitude=51.5074&longitude=-0.1278',
+	baseURL: 'http://99.81.186.138:9090/weather?latitude=51.5074&longitude=-0.1278',
 })
 
 const WeatherBar = () => {
