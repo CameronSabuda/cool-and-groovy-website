da016e1ef301fe52394e2a8f660a9acf0d5b81de
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-18 10:44:56 +0100

    added route to NavigationBar component

--- get-your-way-frontend/src/App.js
+++ get-your-way-frontend/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import React from "react";
 import WeatherBar from "./components/WeatherBar/WeatherBar.jsx";
 import NavigationBar from "./components/NavigationBar/NavigationBar.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      <WeatherBar />
+      <Router>
+        <NavigationBar />
+        <WeatherBar />
+      </Router>
     </div>
   );
 }
--- get-your-way-frontend/src/components/NavigationBar/NavigationBar.jsx
+++ get-your-way-frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./NavigationBar.css";
 import { Navbar, Nav, Container, Button, Image } from "react-bootstrap";
+import { Switch, Route, Link } from "react-router-dom";
 import logo from "../../logo.svg";
+// import LandingPage from "components/LandingPage/index.js"
 
 const NavigationBar = () => {
   return (
     <>
       <Container>
         <Navbar bg="light" variant="light" expand="md">
-          <Navbar.Brand href="#home">
+          <Navbar.Brand as={Link} to="/">
             <Image
               className="d-inline-block align-top logo"
               src={logo}
@@ -21,16 +23,37 @@ const NavigationBar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#movies">Movies</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#signup">Signup</Nav.Link>
-              <Button variant="primary" size="md">
+              <Nav.Link as={Link} to="/movies">
+                Movies
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/signup">
+                Signup
+              </Nav.Link>
+              <Button variant="primary" size="md" as={Link} to="/calltoaction">
                 Call to action
               </Button>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </Container>
+      <Switch>
+        <Route path="/">{/* <LandingPage /> */}</Route>
+        {/* <Route path="/movies">
+          <MoviePage />
+        </Route>
+        <Route path="/about">
+          <MoviePage />
+        </Route>
+        <Route path="/signup">
+          <MoviePage />
+        </Route>
+        <Route path="/calltoaction">
+          <MoviePage />
+        </Route> */}
+      </Switch>
     </>
   );
 };
