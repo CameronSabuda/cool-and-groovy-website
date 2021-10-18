cd51221e047d44c4c13a621cf1d31815992cf1d2
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-18 16:43:54 +0100

    tried to make the layout responsive, but not done yet

--- get-your-way-frontend/src/components/LandingPage/Elements/Hero.jsx
+++ get-your-way-frontend/src/components/LandingPage/Elements/Hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
 			<Container style={ {height: '100vh'} }>
 				<Row className='d-flex justify-content-center py-4 px-4'>
 					{/* hero image */}
-					<Col md={6} className="mx-2 ">
+					<Col xs={6} md={8} className="sm-12 mx-2">
 						<Image src='https://picsum.photos/1200/720' rounded fluid/>
 					</Col>
 					{/* Tagline */}
-					<Col className='d-flex align-items-center px-5'>
+					<Col xs={6} md={4} className='d-flex align-items-center px-5'>
 						<h1>Discover movie scenes around the world</h1>
 					</Col>
 				</Row>
