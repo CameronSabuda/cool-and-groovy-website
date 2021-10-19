420ef726ad67acd8fe61c1b2fdbaa39aa479387b
rickyli-ncl <ricky.li.ncl@gmail.com>
2021-10-19 12:47:29 +0100

    added MovieListPage component

--- get-your-way-frontend/src/App.js
+++ get-your-way-frontend/src/App.js
@@ -2,22 +2,19 @@ import "./App.css";
 import React from "react";
 import WeatherBar from "./components/WeatherBar/WeatherBar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import EventsBar from './components/EventsBar/EventsBar';
+import EventsBar from "./components/EventsBar/EventsBar";
 import NavigationBar from "./components/NavigationBar/NavigationBar.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import MovieListPage from "./components/MovieListPage/index.jsx";
 
 function App() {
-  return (
-    <div className="App">
-
-      <Router>
-        <NavigationBar />
-        <WeatherBar />
-        <EventsBar />
-        <Footer />
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<MovieListPage />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
