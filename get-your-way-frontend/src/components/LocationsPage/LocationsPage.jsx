60fc607504d4bb2e371a5d1a4159468444b29c55
Cameron <cameronsabuda@gmail.com>
2021-10-20 12:55:16 +0100

    Initial map component made

--- get-your-way-frontend/src/components/LocationsPage/LocationsPage.jsx
+++ get-your-way-frontend/src/components/LocationsPage/LocationsPage.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Container, Row, Col} from 'react-bootstrap'
 import CandyStore from '../CandyStore/CandyStore'
 import './LocationsPage.css'
+import Map from '../Map/Map'
+
+
 
 const LocationsPage = () => {
+
+	const [chosenLocation, setChosenLocation] = useState();
+
+	const getLocationFromMap = (location) => {
+		setChosenLocation(location)
+	}
+
+	useEffect(() => {
+		console.log(chosenLocation)
+		return () => {
+			//
+		}
+	}, [chosenLocation])
+
 	return (
 		
 			<Container fluid>
 				<Row >
 					<Col sm={7}>
-						map goes here
+						<div>
+						<Map onClick={getLocationFromMap}/>
+						</div>
 					</Col>
 					<Col sm={5}>
-						<CandyStore />
+						<CandyStore location={chosenLocation}/>
 					</Col>
 				</Row>
 			</Container>
