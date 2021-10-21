2ce99178d89d5d3f647c4e4449f55025fa9f5183
Ryan Lee <ryan.lee2@sky.uk>
2021-10-21 17:21:04 +0100

    Added example picture for landing page

--- get-your-way-frontend/src/components/LandingPage/Elements/Examples.jsx
+++ get-your-way-frontend/src/components/LandingPage/Elements/Examples.jsx
@@ -1,42 +1,46 @@
-import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import React from 'react'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 
 const Examples = () => {
 	return (
-		<div id="about-us" className='bg-light py-5'>
+		<div id='about-us' className='bg-light py-5'>
 			<Container>
 				<Row className='d-flex justify-content-center py-4 px-4'>
-					<Col className="d-flex align-items-center">
+					<Col className='d-flex align-items-center'>
 						<Image
-							src='https://picsum.photos/800/1200'
+							src='./images/example.png'
 							rounded
 							fluid
 						/>
-
 					</Col>
-					<Col className="d-flex flex-column align-items-center justify-content-center">
+					<Col className='d-flex flex-column align-items-center justify-content-center'>
 						<Row>
 							<h2>What we do</h2>
 						</Row>
-						<Row className="">
+						<Row className=''>
 							<p>
-								Using GetYourWay you can get real, live info on the locations that 
-								your favourite films have been shot in.
+								Using GetYourWay you can get real, live info on
+								the locations that your favourite films have
+								been shot in.
 							</p>
-							<p>By selecting any of the movies listed 
-								on the Movies page, you can see a map of some of the main locations that have
-								been used to shoot the chosen movie.
+							<p>
+								By selecting any of the movies listed on the
+								Movies page, you can see a map of some of the
+								main locations that have been used to shoot the
+								chosen movie.
 							</p>
-							<p>Clicking on any of these locations will 
-								update the widgets to the right of the map with all the info to help you decide
-								on whether this place is the location for your next holiday!</p>
-							
+							<p>
+								Clicking on any of these locations will update
+								the widgets to the right of the map with all the
+								info to help you decide on whether this place is
+								the location for your next holiday!
+							</p>
 						</Row>
 					</Col>
 				</Row>
 			</Container>
 		</div>
-	);
-};
+	)
+}
 
-export default Examples;
+export default Examples
